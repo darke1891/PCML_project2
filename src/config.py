248ef606e890f386139cb3593806484a59cfc8d0
starcalm <junzebao@gmail.com>
2016-12-18 13:57:42 +0100

    add three control variables

--- src/config.py
+++ src/config.py
@@ -2,19 +2,20 @@ NUM_CHANNELS = 3 # RGB images
 PIXEL_DEPTH = 255
 NUM_LABELS = 2
 TRAIN_START = 1
-TRAIN_SIZE = 100
+TRAIN_SIZE = 80
 VALIDATION_SIZE = 5  # Size of the validation set.
-SEED = 66478  # Set to None for random seed.
+SEED = None  # Set to None for random seed.
 BATCH_SIZE = 16 # 64
-NUM_EPOCHS = 10
+NUM_EPOCHS = 5
 RECORDING_STEP = 1000
 TRAIN_FORMAT = 'data/training/images/satImage_{:03d}.png'
 TEST_FORMAT = 'data/test_set_images/test_{}.png'
 TEST_START = 1
 TEST_SIZE = 50
 TRAIN_LABEL_FORMAT = 'data/training/groundtruth/satImage_{:03d}.png'
-TRAIN_MODE = False
-RESTORE_MODEL = True # If True, restore existing model instead of training a new one
+TRAIN_MODEL = True # if it's training(including doing predictions on remaining train images) 
+RESTORE_MODEL = True # restore model from backup  
+TEST = False # do predictions with test_set_images
 
 # Set image patch size in pixels
 # IMG_PATCH_SIZE should be a multiple of 4
--- src/tf_aerial_images.py
+++ src/tf_aerial_images.py
@@ -16,12 +16,13 @@ import tensorflow as tf
 from PIL import Image
 
 from config import *
-from basic_read import read_images
+from basic_read import read_images, extract_labels
 from train_read import extract_train
 from test_read import extract_test_labels, extract_test_data
 from test_write import save_image
+from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/mnist',
+tf.app.flags.DEFINE_string('train_dir', '/tmp/mnist-tmp',   # don't use default folder
                            """Directory where to write event logs """
                            """and checkpoint.""")
 FLAGS = tf.app.flags.FLAGS
@@ -80,7 +81,7 @@ def get_image_summary_3d(img):
 
 # We will replicate the model structure for the training subgraph, as well
 # as the evaluation subgraphs, while sharing the trainable parameters.
-def model(data, train, **kwargs):
+def model(data, train, kwargs):
     """The Model definition."""
     # 2D convolution, with 'SAME' padding (i.e. the output feature map has
     # the same size as the input). Note that {strides} is a 4D array whose
@@ -133,20 +134,20 @@ def model(data, train, **kwargs):
     if train:
         summary_id = '_0'
         s_data = get_image_summary(data)
-        filter_summary0 = tf.image_summary('summary_data' + summary_id, s_data)
+        filter_summary0 = tf.summary.image('summary_data' + summary_id, s_data)
         s_conv = get_image_summary(conv)
-        filter_summary2 = tf.image_summary('summary_conv' + summary_id, s_conv)
+        filter_summary2 = tf.summary.image('summary_conv' + summary_id, s_conv)
         s_pool = get_image_summary(pool)
-        filter_summary3 = tf.image_summary('summary_pool' + summary_id, s_pool)
+        filter_summary3 = tf.summary.image('summary_pool' + summary_id, s_pool)
         s_conv2 = get_image_summary(conv2)
-        filter_summary4 = tf.image_summary('summary_conv2' + summary_id, s_conv2)
+        filter_summary4 = tf.summary.image('summary_conv2' + summary_id, s_conv2)
         s_pool2 = get_image_summary(pool2)
-        filter_summary5 = tf.image_summary('summary_pool2' + summary_id, s_pool2)
+        filter_summary5 = tf.summary.image('summary_pool2' + summary_id, s_pool2)
 
     return out
 
 
-def main(args=None):  # pylint: disable=unused-argument
+def train(s, saver, all_params):
 
     # Extract it into np arrays.
     train_data, train_labels = extract_train()
@@ -183,6 +184,7 @@ def main(args=None):  # pylint: disable=unused-argument
         print ('Number of data points per class: c0 = ' + str(c0) + ' c1 = ' + str(c1))
 
     train_size = train_labels.shape[0]
+    print('Training size is {}'.format(train_size))
 
     # This is where training samples and labels are fed to the graph.
     # These placeholder nodes will be fed a batch of training data at each
@@ -196,60 +198,35 @@ def main(args=None):  # pylint: disable=unused-argument
     train_all_data_place = tf.placeholder(tf.float32, shape=train_data.shape)
     train_all_data_node = tf.Variable(train_all_data_place)
 
-    # The variables below hold all the trainable weights. They are passed an
-    # initial value which will be assigned when when we call:
-    # {tf.initialize_all_variables().run()}
-    conv1_weights = tf.Variable(
-        tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
-                            stddev=0.1,
-                            seed=SEED))
-    conv1_biases = tf.Variable(tf.zeros([32]))
-    conv2_weights = tf.Variable(
-        tf.truncated_normal([5, 5, 32, 64],
-                            stddev=0.1,
-                            seed=SEED))
-    conv2_biases = tf.Variable(tf.constant(0.1, shape=[64]))
-    fc1_weights = tf.Variable(  # fully connected, depth 512.
-        tf.truncated_normal([int(IMG_PATCH_SIZE / 4 * IMG_PATCH_SIZE / 4 * 64), 512],
-                            stddev=0.1,
-                            seed=SEED))
-    fc1_biases = tf.Variable(tf.constant(0.1, shape=[512]))
-    fc2_weights = tf.Variable(
-        tf.truncated_normal([512, NUM_LABELS],
-                            stddev=0.1,
-                            seed=SEED))
-    fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS]))
-
-    all_params_node = [conv1_weights, conv1_biases, conv2_weights, conv2_biases, fc1_weights, fc1_biases, fc2_weights,
-                       fc2_biases]
-    all_params_names = ['conv1_weights', 'conv1_biases', 'conv2_weights', 'conv2_biases', 'fc1_weights', 'fc1_biases',
-                        'fc2_weights', 'fc2_biases']
-
-    all_params = dict(zip(all_params_names, all_params_node))
-
     # Training computation: logits + cross-entropy loss.
-    logits = model(train_data_node, True, **all_params) # BATCH_SIZE*NUM_LABELS
+    logits = model(train_data_node, True, all_params) # BATCH_SIZE*NUM_LABELS
     # print 'logits = ' + str(logits.get_shape()) + ' train_labels_node = ' + str(train_labels_node.get_shape())
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
         logits, train_labels_node))
-    tf.scalar_summary('loss', loss)
+    tf.summary.scalar('loss', loss)
 
-    all_grads_node = tf.gradients(loss, all_params_node)
+    all_params_names = list(all_params.keys())
+    all_params_nodes = list(all_params.values())
+    all_grads_node = tf.gradients(loss, all_params_nodes)
     all_grad_norms_node = []
     for i in range(0, len(all_grads_node)):
         norm_grad_i = tf.global_norm([all_grads_node[i]])
         all_grad_norms_node.append(norm_grad_i)
-        tf.scalar_summary(all_params_names[i], norm_grad_i)
+        tf.summary.scalar(all_params_names[i], norm_grad_i)
     
     # L2 regularization for the fully connected parameters.
-    regularizers = (tf.nn.l2_loss(fc1_weights) + tf.nn.l2_loss(fc1_biases) +
-                    tf.nn.l2_loss(fc2_weights) + tf.nn.l2_loss(fc2_biases))
+    regularizers = (
+            tf.nn.l2_loss(all_params['fc1_weights']) +
+            tf.nn.l2_loss(all_params['fc1_biases']) +
+            tf.nn.l2_loss(all_params['fc2_weights']) + 
+            tf.nn.l2_loss(all_params['fc2_biases']))
     # Add the regularization term to the loss.
     loss += 5e-4 * regularizers
 
     # Optimizer: set up a variable that's incremented once per batch and
     # controls the learning rate decay.
     batch = tf.Variable(0)
+
     # Decay once per epoch, using an exponential schedule starting at 0.01.
     learning_rate = tf.train.exponential_decay(
         0.01,                # Base learning rate.
@@ -257,123 +234,179 @@ def main(args=None):  # pylint: disable=unused-argument
         train_size,          # Decay step.
         0.95,                # Decay rate.
         staircase=True)
-    tf.scalar_summary('learning_rate', learning_rate)
+    tf.summary.scalar('learning_rate', learning_rate)
     
     # Use simple momentum for the optimization.
-    optimizer = tf.train.MomentumOptimizer(learning_rate,
-                                           0.0).minimize(loss,
-                                                         global_step=batch)
+    optimizer = tf.train.MomentumOptimizer(learning_rate, 0.0).minimize(loss, global_step=batch)
 
     # Predictions for the minibatch, validation set and test set.
     train_prediction = tf.nn.softmax(logits)
     # We'll compute them only once in a while by calling their {eval()} method.
-    train_all_prediction = tf.nn.softmax(model(train_all_data_node, False, **all_params))
+    train_all_prediction = tf.nn.softmax(model(train_all_data_node, False, all_params))
 
-    # Add ops to save and restore all the variables.
-    saver = tf.train.Saver()
+    # Build the summary operation based on the TF collection of Summaries.
+    summary_op = tf.merge_all_summaries()
+    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
+                                            graph_def=s.graph_def)
+    # If variables are not restored, 
+    # run all the initializers to prepare the trainable parameters.
+    if RESTORE_MODEL:
+        all_vars = tf.all_variables()
+        uninit_vars = [var for var in all_vars if not tf.is_variable_initialized(var).eval()]
+        print('The following are uninitialized variables. Start initing them ')
+        print([var.name for var in uninit_vars])
+        tf.initialize_variables(uninit_vars).run(
+                feed_dict={train_all_data_place: train_data})
+    else:
+        tf.initialize_all_variables().run(feed_dict={train_all_data_place: train_data})
 
-    # Create a local session to run this computation.
+    print ('Initialized!')
+    # Loop through training steps.
+    print ('Total number of iterations = ' + str(int(num_epochs * train_size / BATCH_SIZE)))
+
+    training_indices = range(train_size)
+
+    for iepoch in range(num_epochs):
+
+        # Permute training indices
+        perm_indices = np.random.permutation(training_indices)
+
+        for step in range (int(train_size / BATCH_SIZE)):
+
+            offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
+            batch_indices = perm_indices[offset:(offset + BATCH_SIZE)]
+
+            # Compute the offset of the current minibatch in the data.
+            # Note that we could use better randomization across epochs.
+            batch_data = train_data[batch_indices, :, :, :]
+            batch_labels = train_labels[batch_indices]
+            # This dictionary maps the batch data (as a np array) to the
+            # node in the graph is should be fed to.
+            feed_dict = {train_data_node: batch_data,
+                         train_labels_node: batch_labels}
+
+            if step % RECORDING_STEP == 0:
+
+                summary_str, _, l, lr, predictions = s.run(
+                    [summary_op, optimizer, loss, learning_rate, train_prediction],
+                    feed_dict=feed_dict)
+                #summary_str = s.run(summary_op, feed_dict=feed_dict)
+                summary_writer.add_summary(summary_str, step)
+                summary_writer.flush()
+
+                # print_predictions(predictions, batch_labels)
+
+                print ('Epoch {:2f}, {}/{}' .format(float(step) * BATCH_SIZE / train_size, iepoch, num_epochs))
+                print ('Minibatch loss: %.3f, learning rate: %.6f' % (l, lr))
+                print ('Minibatch error: %.1f%%' % error_rate(predictions,
+                                                             batch_labels))
+
+                sys.stdout.flush()
+            else:
+                # Run the graph and fetch some of the nodes.
+                _, l, lr, predictions = s.run(
+                    [optimizer, loss, learning_rate, train_prediction],
+                    feed_dict=feed_dict)
+
+    # Save the variables to disk.
+    # save the last one
+    save_path = saver.save(s, FLAGS.train_dir + "/model.ckpt")
+    print("Model saved in file: %s" % save_path)
+
+    # do predictions on remaining training data
+    remaining_train_size = 100 - TRAIN_SIZE
+    if remaining_train_size != 0:
+        remaining_start = TRAIN_SIZE + 1
+        print("""Cross validation on train data.\nRemaining {} train images will 
+                be used for testing""".format(remaining_train_size))
+
+        test(s, all_params, TRAIN_FORMAT, remaining_start, remaining_train_size)
+
+
+def test(s, all_params, data_format, index_start, size):
+    images = extract_test_data(data_format, index_start, size)
+    is_cv = 'train' in data_format
+    prediction_dir = "predictions_test/"
+    if is_cv:
+        prediction_dir = 'predictions_train/'
+        labels = extract_labels(data_format, index_start, size, False)
+    
+    if not os.path.isdir(prediction_dir):
+        os.mkdir(prediction_dir)
+
+    output_predictions = np.zeros((0, NUM_LABELS))
+    for index, image_data in enumerate(images):
+        img = image_data[0]
+        img_patches = image_data[1]
+        # predicting
+        data_node = tf.constant(img_patches)
+        output = tf.nn.softmax(model(data_node, False, all_params))
+        prediction = s.run(output)
+        output_predictions = np.concatenate((output_predictions, prediction))
+
+        save_image(img, prediction, prediction_dir, index + index_start)
+
+    if is_cv:
+        print('Cross validation error rate: {}'.format(
+            error_rate(output_predictions, labels)))
+
+
+def main(args=None):
+    # The variables below hold all the trainable weights. They are passed an
+    # initial value which will be assigned when when we call:
+    # {tf.initialize_all_variables().run()}
+    conv1_weights = tf.Variable(
+        tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
+                            stddev=0.1,
+                            seed=SEED),
+        name='conv1_weights')
+    conv1_biases = tf.Variable(tf.zeros([32]), name='conv1_biases')
+    conv2_weights = tf.Variable(
+        tf.truncated_normal([5, 5, 32, 64],
+                            stddev=0.1,
+                            seed=SEED),
+        name='conv2_weights')
+    conv2_biases = tf.Variable(tf.constant(0.1, shape=[64]), name='conv2_biases')
+    fc1_weights = tf.Variable(  # fully connected, depth 512.
+        tf.truncated_normal([int(IMG_PATCH_SIZE / 4 * IMG_PATCH_SIZE / 4 * 64), 512],
+                            stddev=0.1,
+                            seed=SEED),
+        name='fc1_weights')
+    fc1_biases = tf.Variable(tf.constant(0.1, shape=[512]), name='fc1_biases')
+    fc2_weights = tf.Variable( 
+        tf.truncated_normal([512, NUM_LABELS],
+                            stddev=0.1,
+                            seed=SEED),
+        name='fc2_weigths')
+    fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS]), name='fc2_biases')
+
+
+    all_params_node = [conv1_weights, conv1_biases, conv2_weights, conv2_biases, fc1_weights, fc1_biases, fc2_weights,
+                       fc2_biases]
+    all_params_names = ['conv1_weights', 'conv1_biases', 'conv2_weights', 'conv2_biases', 'fc1_weights', 'fc1_biases',
+                        'fc2_weights', 'fc2_biases']
+    all_params = dict(zip(all_params_names, all_params_node))
+
+    saver = tf.train.Saver() 
+
     with tf.Session(
             config=tf.ConfigProto(
                 intra_op_parallelism_threads=10,
                 inter_op_parallelism_threads=10,
                 use_per_session_threads=True)
             ) as s:
-
+        # read saved model
         if RESTORE_MODEL:
-            # Restore variables from disk.
             saver.restore(s, FLAGS.train_dir + "/model.ckpt")
-            print("Model restored.")
-
-        else:
-            # Run all the initializers to prepare the trainable parameters.
-            tf.initialize_all_variables().run(feed_dict={train_all_data_place: train_data})
-
-            # Build the summary operation based on the TF collection of Summaries.
-            summary_op = tf.merge_all_summaries()
-            summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
-                                                    graph_def=s.graph_def)
-            print ('Initialized!')
-            # Loop through training steps.
-            print ('Total number of iterations = ' + str(int(num_epochs * train_size / BATCH_SIZE)))
-
-            training_indices = range(train_size)
-
-            for iepoch in range(num_epochs):
-
-                # Permute training indices
-                perm_indices = np.random.permutation(training_indices)
-
-                for step in range (int(train_size / BATCH_SIZE)):
-
-                    offset = (step * BATCH_SIZE) % (train_size - BATCH_SIZE)
-                    batch_indices = perm_indices[offset:(offset + BATCH_SIZE)]
-
-                    # Compute the offset of the current minibatch in the data.
-                    # Note that we could use better randomization across epochs.
-                    batch_data = train_data[batch_indices, :, :, :]
-                    batch_labels = train_labels[batch_indices]
-                    # This dictionary maps the batch data (as a np array) to the
-                    # node in the graph is should be fed to.
-                    feed_dict = {train_data_node: batch_data,
-                                 train_labels_node: batch_labels}
-
-                    if step % RECORDING_STEP == 0:
-
-                        summary_str, _, l, lr, predictions = s.run(
-                            [summary_op, optimizer, loss, learning_rate, train_prediction],
-                            feed_dict=feed_dict)
-                        #summary_str = s.run(summary_op, feed_dict=feed_dict)
-                        summary_writer.add_summary(summary_str, step)
-                        summary_writer.flush()
-
-                        # print_predictions(predictions, batch_labels)
-
-                        print ('Epoch {:2f}, {}/{}' .format(float(step) * BATCH_SIZE / train_size, iepoch, num_epochs))
-                        print ('Minibatch loss: %.3f, learning rate: %.6f' % (l, lr))
-                        print ('Minibatch error: %.1f%%' % error_rate(predictions,
-                                                                     batch_labels))
-
-                        sys.stdout.flush()
-                    else:
-                        # Run the graph and fetch some of the nodes.
-                        _, l, lr, predictions = s.run(
-                            [optimizer, loss, learning_rate, train_prediction],
-                            feed_dict=feed_dict)
-
-                # Save the variables to disk.
-                save_path = saver.save(s, FLAGS.train_dir + "/model.ckpt")
-                print("Model saved in file: %s" % save_path)
-
-        if TRAIN_MODE:
-            print("Running prediction on training set")
-            prediction_dir = "predictions_training/"
-            index_start = TRAIN_START
-            images = extract_test_data(TRAIN_FORMAT, index_start, TRAIN_SIZE)
-        else:
-            print('Running prediction on test dataset')
-            prediction_dir = 'predictions_test/'
-            index_start = TEST_START
-            images = extract_test_data(TEST_FORMAT, index_start, TEST_SIZE)
-        
-        if not os.path.isdir(prediction_dir):
-            os.mkdir(prediction_dir)
-
-        output_predictions = np.zeros((0, NUM_LABELS))
-        for index, image_data in enumerate(images):
-            img = image_data[0]
-            img_patches = image_data[1]
-            # predicting
-            data_node = tf.constant(img_patches)
-            output = tf.nn.softmax(model(data_node, False, **all_params))
-            prediction = s.run(output)
-            output_predictions = np.concatenate((output_predictions, prediction))
-
-            save_image(img, prediction, prediction_dir, index + index_start)
-        
-        if TRAIN_MODE:
-            labels = extract_test_labels()
-            print('Error rate: {}'.format(error_rate(output_predictions, labels)))
+            print('Model Restored! The following variables are stored: ')
+            print_tensors_in_checkpoint_file(FLAGS.train_dir + '/model.ckpt', False)
+        # train the saved model or a new model
+        # including cross validation on 100-TRAIN_SIZE images
+        if TRAIN_MODEL:
+            train(s, saver, all_params)
+        # predicting on test data
+        if TEST:
+            test(s, all_params, TEST_FORMAT, TEST_START, TEST_SIZE)
 
 
 if __name__ == '__main__':
